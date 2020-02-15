17e801188d7a7dd2d6b9354579cebbadbfde521d
Sunilv390 <sunilv390@gmail.com>
2020-02-15 15:18:35 +0530

    Added: Read inputs for three values

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,1 +1,7 @@
-echo "WELCOME TO ARITHMETIC PROBLEMS"
+#!/bin/bash -x
+
+#USERINPUT
+read -p "Enter value for a " number1
+read -p "Enter value for b " number2
+read -p "Enter value for c " number3
+
