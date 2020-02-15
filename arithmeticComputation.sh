1e02cbb251b84152c59b3e0f87727f2d20d662b2
Sunilv390 <sunilv390@gmail.com>
2020-02-15 16:43:09 +0530

    Modified: operation third to print decimal value

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -8,5 +8,5 @@ read -p "Enter value for c " number3
 #ARITHMETIC OPERATIONS
 operation1=$(($number1+$number2*$number3))
 operation2=$(($number1*$number2+$number3))
-operation3=$(($number3+$number1/$number2))
+operation3=`echo "scale=2;$number3+$number1/$number2" | bc`
 echo "Operations are $operation1 $operation2 $operation3"
