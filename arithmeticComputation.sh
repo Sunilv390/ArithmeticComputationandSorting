b589cac022e8120890df8ce4fe585dd0b08ae2d8
Sunilv390 <sunilv390@gmail.com>
2020-02-15 16:08:48 +0530

    Added: basic Arithmetic problem

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -8,5 +8,5 @@ read -p "Enter value for c " number3
 #ARITHMETIC OPERATIONS
 operation1=$(($number1+$number2*$number3))
 operation2=$(($number1*$number2+$number3))
-echo "First operation is $operation1 "
-echo "Second operation is $operation2 "
+operation3=$(($number3+$number1/$number2))
+echo "Operations are $operation1 $operation2 $operation3"
