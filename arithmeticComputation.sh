c1b058095580c7b3dd7cad60ff8bee67ca894a14
Sunilv390 <sunilv390@gmail.com>
2020-02-25 19:01:32 +0530

    Refactor:Indentation

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -4,29 +4,23 @@
 read -p "Enter value for a " number1
 read -p "Enter value for b " number2
 read -p "Enter value for c " number3
-
 #ARITHMETIC OPERATIONS
 operation1=`echo "scale=2; $number1+$number2*$number3" | bc`
 operation2=`echo "scale=2; $number1*$number2+$number3" | bc`
 operation3=`echo "scale=2; $number3+$number1/$number2" | bc`
 operation4=`echo "scale=2; $number1%$number2+$number3" | bc`
-echo "Operations are $operation1 $operation2 $operation3 $operation4 "
-
 #STORING OPERATIONS IN DICTIONARY
 declare -A arithmetic
 arithmetic[operation1]=$operation1
 arithmetic[operation2]=$operation2
 arithmetic[operation3]=$operation3
 arithmetic[operation4]=$operation4
-
-#STORING THE OPERATIONS IN AN ARRAY
+#STORING THE OPERATIONS
 length=${#arithmetic[@]}
 for (( index=0; index<$length; index++ ))
 do
-   array[index]=${arithmetic[operation$((index+1))]}
+		array[index]=${arithmetic[operation$((index+1))]}
 done
-echo "Storing the values in an array " ${array[@]}
-
 #SORT THE RESULT IN DECENDING ORDER
 for (( index=0; index<$length; index++ ))
 do
@@ -40,9 +34,7 @@ do
 		fi
 	done
 done
-echo  "Descending values are " ${array[@]}
-
-#SORT THE ARRAY IN ASCENDING ORDER
+#SORT THE RESULT IN ASCENDING ORDER
 for (( index=0; index<$length; index++ ))
 do
    for (( index1=0; index1<$length-1; index1++ ))
@@ -55,4 +47,3 @@ do
       fi
    done
 done
-echo "Ascending values are " ${array[@]}
